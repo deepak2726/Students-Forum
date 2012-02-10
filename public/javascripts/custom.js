d82202a939ce98d902c7bdc967564d9f6e33412d
Yatish Mehta <yatishmehta27@gmail.com>
2012-02-10 21:00:29 +0530

    changes

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,18 +1,19 @@
 jQuery(document).ready(function(){	
 	//Back to top slider
     jQuery('a[href=#totop]').click(function(){
-        jQuery('html, body').animate({scrollTop:0}, 100);
+        jQuery('html, body').animate({scrollTop:0}, 1000);
         return false;
     });
 	// Product Slider
     jQuery('#featured-products').jcarousel();
 
 	// FancyBox jQuery
-	jQuery("a.group").fancybox({ 'zoomSpeedIn': 400, 'zoomSpeedOut': 400, 'overlayShow': true });
+	jQuery("a.group").fancybox({ 'zoomSpeedIn': 600, 'zoomSpeedOut': 600, 'overlayShow': true });
 
 	// Slider Homepage
 	jQuery("#slider").easySlider({
 		auto: true,
+                pause:7000,
 		continuous: true,
 		numeric: true
 	});
